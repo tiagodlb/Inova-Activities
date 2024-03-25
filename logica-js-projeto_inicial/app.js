5464cd6638edeaf5de992561146c2ecac1c49f51
tiago <tigolimab223@gmail.com>
2024-03-25 12:28:37 -0300

    feat: random attempts

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,8 @@
 alert("Boas vindas ao nosso site!");
 
-const secretNumber = 29;
+const secretNumber = parseInt((Math.random()) * 30 + 1);
+console.log(secretNumber)
+let numberAttempt = 1;
 game();
 
 function extra() {
@@ -16,13 +18,16 @@ function extra() {
 function game() {
     let isTrue = false;
     let attempt = parseInt(prompt("Escolha um número entre 1 e 30"));
-    switch (attempt) {
-        case 29:
-            alert(`Você acertou, o número secreto é ${secretNumber}`);
-            isTrue = true;
-            break;
-        default:
-            alert("Você errou :(");
+    if (attempt < secretNumber) {
+        alert("O seu chute é menor que o número secreto");
+        numberAttempt++;
+    } else if (attempt > secretNumber) {
+        alert("O seu chute é maior que o número secreto");
+        numberAttempt++;
+    } else {
+        alert(`Você acertou em ${numberAttempt} ${numberAttempt > 1 ? 'tentativas' : 'tentativa'}! O número secreto era ${secretNumber}`);
+        isTrue = true;
     }
     if (!isTrue) game();
+    return;
 }
